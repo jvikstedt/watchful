91c4a891d4b9718da9ad3a3b8f97d7ceb3034325
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-10-12 19:01:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Watchful
+===================
+
+Watchful is job runner and notifier application.
+
+----------
+#### Technologies
+
+Go (Backend)
+
+Vue.js (Client)
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
